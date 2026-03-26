4bba6c1406e877b470552186113e89c885773760
BharathikN31 <bharathi311003@gmail.com>
2026-03-26 23:46:36 +0530

    Added EF core and DB in SSMS

--- EWTS.API/Program.cs
+++ EWTS.API/Program.cs
@@ -1,9 +1,21 @@
+using Microsoft.EntityFrameworkCore;
+using EWTS.Infrastructure.Persistence;
+using EWTS.Domain.Interfaces;
+using EWTS.Infrastructure.Repositories;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddScoped<IUserRepository, UserRepository>();
+
 
 var app = builder.Build();
 
---
+++ EWTS.Infrastructure/Repositories/UserRepository.cs
@@ -0,0 +1,33 @@
+using EWTS.Domain.Entities;
+using EWTS.Domain.Interfaces;
+using EWTS.Infrastructure.Persistence;
+using Microsoft.EntityFrameworkCore;
+
+namespace EWTS.Infrastructure.Repositories
+{
+    public class UserRepository : IUserRepository
+    {
+        private readonly AppDbContext _context;
+
+        public UserRepository(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<User> GetByIdAsync(Guid id)
+        {
+            return await _context.Users.FindAsync(id);
+        }
+
+        public async Task<User> GetByEmailAsync(string email)
+        {
+            return await _context.Users.FirstOrDefaultAsync(x => x.Email == email);
+        }
+
+        public async Task AddAsync(User user)
+        {
+            await _context.Users.AddAsync(user);
+            await _context.SaveChangesAsync();
+        }
+    }
+}
